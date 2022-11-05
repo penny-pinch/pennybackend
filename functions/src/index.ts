4f5cd5bbfc340428e56f0af488b9957661a22a3e
klavsinthehouse <88055533+klavsinthehouse@users.noreply.github.com>
2022-11-05 09:31:47 +0200

    Appliance start enabled

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,3 +2,5 @@ export {iotHandler} from "./services/iot";
 export {deviceToggle} from "./services/device-toggle";
 export {priceConsumerScheduler} from "./services/price-consumer";
 export {notificationsScheduler} from "./services/notifications-provider";
+export {applianceCycleTrigger}
+  from "./services/appliance-cycles/startApplianceCycle";
--- functions/src/services/appliance-cycles/startApplianceCycle.ts
+++ functions/src/services/appliance-cycles/startApplianceCycle.ts
@@ -1,4 +1,61 @@
-export const startApplianceCycle = (props: any) => {
+import {db} from "../firebase/firebase";
+import * as functions from "firebase-functions";
+
+export const applianceCycleTrigger =
+  functions.https.onRequest(async (request, response) => {
+    const priceRef = db.doc("prices/49wuk0lqwKyWMADWGPUv");
+    const priceSnap = await priceRef.get();
+    const priceData = priceSnap.data();
+    await startApplianceCycle({
+      appliance: {
+        name: "Washing machine",
+        cycleLengthHours: 2.5,
+        consumption: 0.8,
+      },
+      prices: priceData,
+      startTime: 10,
+    });
+    response.send("Success");
+  });
+
+
+export const startApplianceCycle = async (props: any) => {
   const {appliance, prices, startTime} = props;
-  return {appliance, prices, startTime};
+  const startPriceIndex = Math.floor(startTime);
+  let cost = 0;
+  const averageCost =
+    appliance.cycleLengthHours *
+    prices.averagePrice *
+    appliance.consumption;
+  const highestPrice = Math.max(...prices.prices.map((o:any) => o.price));
+  const highestCost =
+    highestPrice *
+    appliance.consumption *
+    appliance.cycleLengthHours;
+
+  const overflowInNextHour = appliance.cycleLengthHours % 1;
+  for (
+    let i = startPriceIndex;
+    i < startPriceIndex + Math.floor(appliance.cycleLengthHours);
+    i++) {
+    cost += (prices.prices[i].price * appliance.consumption);
+  }
+  if (overflowInNextHour > 0) {
+    const overflowPrice = prices.prices[startPriceIndex +
+      Math.ceil(appliance.cycleLengthHours)].price * overflowInNextHour;
+    cost += (overflowPrice * appliance.consumption);
+  }
+
+  const applianceCycle = {
+    appliance: appliance.name,
+    cost,
+    differenceFromAverage: averageCost - cost,
+    differenceFromHighest: highestCost - cost,
+    duration: appliance.cycleLengthHours,
+  };
+
+  await db
+      .collection("applianceCycles")
+      .add(applianceCycle);
+  return;
 };
